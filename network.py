4e46df363474c1dec2e29d8b60e6313ad34ea9a6
Sleightly <ejc042@ucsd.edu>
2019-02-26 18:01:09 -0800

    updated to contain combinations

--- network.py
+++ network.py
@@ -57,30 +57,62 @@ def buildGraph():
     for plugin in q2methods:
         method_list = plugin[1]
         for method in method_list:
-            if not G.has_node(method):
-                G.add_node(method,value=method, color='red')
+            #check if there are any combinations in parameters
             req_in, non_req_in = getNextParam(method,1)
-            if non_req_in:
-                print("COMBINATIONS: ")
-                print(getCombinations(non_req_in))
-            for key in req_in:
-                if not G.has_node(key):
-                    G.add_node(key, value=key, color='blue')
-                G.add_edge(key, method)
-            for key in req_in:
-                if not G.has_node(key):
-                    G.add_node(key, value=key, color='blue')
-                G.add_edge(key, method)
-
             req_out, non_req_out = getNextParam(method,0)
+
+            #add keys to graph
+            for key in req_in:
+                if not G.has_node(key):
+                    G.add_node(key, value=key, color='blue')
+            for key in non_req_in:
+                if not G.has_node(key):
+                    G.add_node(key, value=key, color='blue')
             for key in req_out:
                 if not G.has_node(key):
                     G.add_node(key, value=key, color='blue')
-                G.add_edge(method, key)
             for key in non_req_out:
                 if not G.has_node(key):
                     G.add_node(key, value=key, color='blue')
-                G.add_edge(method, key)
+
+            #there are combinations
+            if non_req_in:
+                #construct combinations
+                print("COMBINATIONS: ")
+                print(getCombinations(non_req_in))
+                combs = getCombinations(non_req_in)
+
+                for non_req in combs:
+                    print("DEBUG")
+                    print(non_req)
+                    new_method_key= str(method)
+                    str_non_req = [str(i) for i in non_req]
+                    if str_non_req:
+                        non_req_key = ";".join(str_non_req)
+                        new_method_key += ";"+non_req_key
+                    print("new method"+new_method_key)
+
+                    #check if there exists a method node with same in-edges
+                    if not G.has_node(new_method_key):
+                        G.add_node(new_method_key,value=method,color='red')
+                        for key in req_in:
+                            G.add_edge(key, new_method_key)
+                        for key in non_req:
+                            G.add_edge(key, new_method_key)
+                        for key in req_out:
+                            G.add_edge(new_method_key, key)
+                        for key in non_req_out:
+                            G.add_edge(new_method_key, key)
+            else:
+                if not G.has_node(method):
+                    G.add_node(method,value=method,color='red')
+                for key in req_in:
+                    G.add_edge(key, method)
+                for key in req_out:
+                    G.add_edge(method, key)
+                for key in non_req_out:
+                    G.add_edge(method, key)
+
 
 buildGraph()
 c_map = [n[1]['color'] for n in list(G.nodes(data=True))]
@@ -89,6 +121,11 @@ print()
 nx.draw(G, node_color=c_map, with_labels=True)
 plt.draw()
 plt.show()
+plt.savefig('graph2.png')
 
 
+for i in G.nodes:
+    print(i)
+    print(G.in_edges(nbunch=i))
+    print()
 
